343e926489f156cf2f3013bcc5c038098e1fc14a
Creator912 <walker.joshua912@yahoo.com>
2023-12-13 13:48:10 -0800

    Finish emojis

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react"
+import Square from './components/Square'
 import "./App.css"
 
+// V declaration V
 const App = () => {
   const [board, setBoard] = useState([
-    "?",
-    "?",
+    "❌",
+    "🔪",
     "?",
     "?",
     "?",
@@ -13,12 +15,22 @@ const App = () => {
     "?",
     "?"
   ])
+// console.log("board:", board);
+// V New function must be done before return V
+const handleClick = (id) => {
+  alert(id)
+}
+
 
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <Square board={board} handleClick={handleClick}/>
     </>
   )
 }
 
 export default App
+
+
+
